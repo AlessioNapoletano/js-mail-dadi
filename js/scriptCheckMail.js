e6fb183d2c7fd3111e08397891477c1b1a323b77
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-24 16:09:33 +0100

    Modified

--- js/scriptCheckMail.js
+++ js/scriptCheckMail.js
@@ -6,7 +6,14 @@ PROGRAMMA 2 - Mail
 */
 
 //Lista di email nel presunto DB
-const listEmail = ["alessio.napoletano@cosaguardi.com", "alessio.napoli@ancoracheguardi.net"];
+const listEmail = [
+    "alessio.napoletano@cosaguardi.com",
+    "alessio.napoli@ancoracheguardi.net",
+    "alessio.napoli@vabbuòUFratedavveroFai.org" , 
+    "alessio.napoli@ammaFaETarantelle.org"
+];
+
+console.log("lista delle mail disponibili: " + listEmail);
 
 
 const button = document.querySelector("a.btn");
@@ -17,27 +24,26 @@ const button = document.querySelector("a.btn");
 const inputEmail = document.getElementById("userEmail");
 
 button.addEventListener("click", function () {
-
+    //variabile con valore 0 o 1 se la varibile vale ' la mail non è stata trovata se vale 1 si
+    let count = 0;
     //prendo il valore del campo input
     const mail = inputEmail.value;
     console.log(mail);
 
-    for (let i = 0; i<listEmail.length; i++){
-        
-        if(mail === listEmail[i]){
-            document.getElementById("output").innerHTML = "Valida!";
-        }else{
-            document.getElementById("output").innerHTML = "Mail non trovata!";
-        }
-    }
+    for (let i = 0; i < listEmail.length; i++) {
 
-    /*
-        if (mail == mailDemo) {
-            document.getElementById("output").innerHTML = "Valida!";
+        if (mail === listEmail[i]) {
+            count++;
+        } else{
+            
+        }
+
+        if (count != 0) {
+            document.getElementById("output").innerHTML = "Mail trovata!";
         } else {
             document.getElementById("output").innerHTML = "Mail non trovata!";
         }
-    */
+    }
 
 });
 
